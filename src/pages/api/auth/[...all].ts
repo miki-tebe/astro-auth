53c8cfb43d68e10e213f1c3f34bfd12379398925
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-09-25 14:37:20 +0300

    move to "auth" directory

--- src/pages/api/auth/[...all].ts
+++ src/pages/api/auth/[...all].ts
@@ -1,7 +1,7 @@
 export const prerender = false;
 
 import type { APIRoute } from "astro";
-import { auth } from "../../lib/auth"; // path to your auth file
+import { auth } from "../../../lib/auth"; // path to your auth file
 
 export const GET: APIRoute = ({ request }) => {
   return auth.handler(request);
