deef60ba716e00d15d18cd216212555228b4d0e3
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-09-26 19:17:10 +0300

    Update auth-client.ts and index.astro

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/client";
+import { createAuthClient as createVanillaClient } from "better-auth/client";
 
 export const client = createAuthClient({
   baseURL: "http://localhost:4321", // the base url of your auth server
 });
+
+export const { useSession: useVanillaSession } = createVanillaClient();
